188dae3c776f43727e80852149e8b8935358f4b4
Nada Badawi <nada_badawi@aucegypt.edu>
2021-04-11 23:58:59 +0200

    Changed in Jerry cpp and header file, added attributes and functions

--- jerry.cpp
+++ jerry.cpp
@@ -14,6 +14,10 @@ Jerry::Jerry(int initialRow, int initialColumn, int d[10][10])
     setPos(50 + 50 * initialColumn, 50 + 50 * initialRow);
     row = initialRow;
     column = initialColumn;
+    withCheese=false;
+    isInvincible=false;
+    no_lives = 3;
+    no_cheese = 0;
 }
 void Jerry::setRow(int newRow)
 { row = newRow; }
@@ -61,19 +65,69 @@ void Jerry::move()
         column--;
     }
     setPos(50 + 50 * column, 50 + 50 * row);
-}
+
+    if (data[row][column] == -20)
+        if (withCheese)
+        {
+            WinLose(no_lives, ++no_cheese);
+            //Function to check on the number of cheese --> if 4 then win
+            //The other swap function swapJerry(); from cheese to normal
+        }
+
 
    QList<QGraphicsItem*> items = collidingItems();
     for (int i = 0; i < items.size(); i++)
     {
-        if (typeid(*items[i]) == typeid(Food))
+        if (typeid(*items[i]) == typeid(Cheese))
         {
             scene()->removeItem(items[i]);
+            swapJerry(*this);
+            withCheese=true;
+        }
+        else if (typeid(*items[i]) == typeid(Pellet))
+        {
+            mode = Invincible;
+            isInvincible =true;
+            //Opacity***
+            //Timer --> 5 seconds to change back to normal --> New function is required to change to normal
+            //Update Life Bar
+            LifeBar();
+        }
+        else if (typeid(*items[i]) == typeid(Tom))
+        {
+            if(!isInvincible)
+            {
+                WinLose(--no_lives, no_cheese);
+                //Function to check on the lives lost --> if 0 then game over
+                //Blink***
+                //Update life bar
+                LifeBar();
+            }
         }
     }
+}
 
-void Jerry:: swapJerry(Jerry J)
+void Jerry:: swapJerry(Jerry& J)
 {
      J.setPixmap(QPixmap("JerrywihCheese"));
+}
+
+void Jerry:: WinLose(int lifeNum, int cheeseNum)
+{
+    if (lifeNum == 0)
+    {
+        //Display "Game Over"
+    }
+    else if (cheeseNum == 4)
+    {
+        //Display "Winner Winner, Chicken Dinner"
+    }
+}
+void Jerry:: LifeBar()
+{
+    //Display number of lives and the number of cheese collected
+    //Can be represented by pics
+
+    //TO UPDATE: 1. Delete old bar 2. Rewrite the new bar
 }
 
--- jerry.h
+++ jerry.h
@@ -1,11 +1,32 @@
 #ifndef JERRY_H
 #define JERRY_H
 
+#include <QObject>
+#include <QGraphicsPixmapItem>
+#include <QKeyEvent>
+#include <QList>
 
-class Jerry
-{
-public:
-    Jerry();
+#include <QGraphicsScene>
+class Jerry: public QObject, public QGraphicsPixmapItem
+{ 
+        Q_OBJECT
+    private:
+        char direction;
+        int row, column;
+        int data[10][10];
+        bool withCheese;
+        bool isInvincible;
+    public:
+        Jerry(int initialRow, int initialColumn, int d[10][10]);
+        void setRow(int newRow);
+        int getRow();
+        void setColumn(int newColumn);
+        int getColumn();
+    public slots:
+        void keyPressEvent(QKeyEvent* event);
+        void move();
+        void swapJerry(Jerry& J);
+
 };
 
 #endif // JERRY_H
